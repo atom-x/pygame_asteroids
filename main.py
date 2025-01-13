857c9c517192db9aeab92879e1df5a22950faf6f
Evgen Temchenko <evgen.temchenko@gmail.com>
2025-01-13 21:21:37 +0100

    Add clock to the loop

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -18,6 +21,7 @@ def main():
                 running = False
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
