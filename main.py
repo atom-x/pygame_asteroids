44e51d2e11c070d8091db385f9ea3d186bc06420
Evgen Temchenko <evgen.temchenko@gmail.com>
2025-01-13 22:03:04 +0100

    Add Groups support

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     clock = pygame.time.Clock()
     dt = 0
 
@@ -25,8 +30,12 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+
+        for entity in updatable:
+            entity.update(dt)
+
+        for entity in drawable:
+            entity.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
